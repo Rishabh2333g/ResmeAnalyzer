d5ddd32902fce944ce9853426c0ea13d24e973ab
Rishabh Gupta <156583510+Rishabh2333g@users.noreply.github.com>
2024-11-24 23:52:45 +0530

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -36,7 +36,7 @@ app.post("/analyze", async (req, res) => {
 });
 
 // Start Server
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });

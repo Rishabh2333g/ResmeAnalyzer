c8ba40bc0fd5bab40862c9f368517765e4586828
Rishabh Gupta <156583510+Rishabh2333g@users.noreply.github.com>
2024-11-24 23:42:25 +0530

    Update ServerAPI.js

--- Client/src/api/ServerAPI.js
+++ Client/src/api/ServerAPI.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const analyzeResume = async (prompt, role) => {
   try {
-    const response = await API.post("https://resmeanalyzer.onrender.com/analyze", { prompt, role });
+    const response = await axios.post("https://resmeanalyzer.onrender.com/analyze", { prompt, role });
     return response.data;
   } catch (error) {
     console.error("Error fetching response from server:", error);
